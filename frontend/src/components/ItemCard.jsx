dcabd6b38d444172d173af7f62cb5a84a629b156
Medhani M.A.T IT24103082 <it24103082@my.sliit.lk>
2026-04-28 11:25:52 +0530

    ItemCard.jsx Edited

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -12,7 +12,7 @@ function ItemCard({ item, onDelete }) {
       <p><strong>Category:</strong> {item.category}</p>
       <p><strong>Price:</strong> ${item.price}</p>
       <p>{item.description}</p>
-      <p><strong>Warrenty Period:</strong> ${item.warrentyPeriod}</p>
+      <p><strong>Warrenty Period:</strong> {item.warrentyPeriod}</p>
 
       <div className="card-actions">
         <Link className="btn secondary" to={`/edit-item/${item._id}`}>Edit</Link>
